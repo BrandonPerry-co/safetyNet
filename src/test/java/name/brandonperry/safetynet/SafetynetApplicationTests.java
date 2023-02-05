b2be6f7ea8a6a9059685b8ee2e7c1ed6a13a73b9
BrandonPerry-co <72050427+BrandonPerry-co@users.noreply.github.com>
2023-02-05 15:59:26 -0600

    added files

---
+++ src/test/java/name/brandonperry/safetynet/SafetynetApplicationTests.java
@@ -0,0 +1,13 @@
+package name.brandonperry.safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
